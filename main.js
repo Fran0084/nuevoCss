a8e908d6a9b7625d95e029ef6d01e1e1cc7bc8d9
Fran Vazquez <fran.vazquezf@hotmail.com>
2024-07-04 17:28:07 -0300

    primerCommit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// let nombre = prompt("Ingrese su nombre");
+
+// switch(nombre){
+//     case "fran": 
+//     document.write("el nombre es correcto");
+//     break;
+
+// case "jose":
+//     document.write("el nombre es incorrecto");
+//     break;
+
+//     case "juan":
+//         document.write("Este nombre tambien es incorrecto");
+//         break;
+
+// default:
+//     document.write("No es ninguno de los 2 nombres inocrrectos");
+
+// }
